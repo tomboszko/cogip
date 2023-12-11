6a964abfd989b88323e087a4c902e119d0447038
Thomas Gomree <gomreethomas@gmail.com>
2023-12-11 16:14:49 +0100

    Add error handling and response codes to API endpoints

--- controllers/WelcomeController.php
+++ controllers/WelcomeController.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../models/InvoicesModel.php';
 require_once __DIR__ . '/../models/CompaniesModel.php';
 require_once __DIR__ . '/../models/ContactsModel.php';
 require_once __DIR__ . '/../models/WelcomeModel.php';
+require_once __DIR__ . '/../models/ErrorModel.php';
 
 class WelcomeController {
     private $model;
@@ -14,39 +15,51 @@ class WelcomeController {
 
 
 public function getLastCompanies() {
+    $error = new ErrorModel();
         try {
             $companies = $this->model->getLastCompanies();
             $companies = array('Last companies' => $companies); // Wrap the companies array inside another array
             header('Content-Type: application/json');
-            echo json_encode($companies, JSON_PRETTY_PRINT);
+            http_response_code(200);
+            echo json_encode([
+                'status' => 200,
+                'data' => $companies], JSON_PRETTY_PRINT);
         } catch (Exception $e) {
-            http_response_code(500);
-            echo json_encode(['message' => 'An error occurred while fetching companies'], JSON_PRETTY_PRINT);
+            $error->logError($e);
+            $error->sendErrorResponse($e);
         }
     }
 
     public function getLastContacts() {
+        $error = new ErrorModel();
         try {
             $contacts = $this->model->getLastContacts();
             $contacts = array('Last contacts' => $contacts); // Wrap the contacts array inside another array
             header('Content-Type: application/json');
-            echo json_encode($contacts, JSON_PRETTY_PRINT);
+            http_response_code(200);
+            echo json_encode([
+                'status' => 200,
+                'data' => $contacts], JSON_PRETTY_PRINT);
         } catch (Exception $e) {
-            http_response_code(500);
-            echo json_encode(['message' => 'An error occurred while fetching contacts'], JSON_PRETTY_PRINT);
+            $error->logError($e);
+            $error->sendErrorResponse($e);
         }
     }
 
-    // Get the last 2 invoices
+    
     public function getLastInvoices() {
+        $error = new ErrorModel();
         try {
             $invoices = $this->model->getLastInvoices();
             $invoices = array('last invoices' => $invoices); // Wrap the invoices array inside another array
             header('Content-Type: application/json');
-            echo json_encode($invoices, JSON_PRETTY_PRINT);
+            http_response_code(200);
+            echo json_encode([
+                'status' => 200,
+                'data' => $invoices], JSON_PRETTY_PRINT);
         } catch (Exception $e) {
-            http_response_code(500);
-            echo json_encode(['message' => 'An error occurred while fetching invoices'], JSON_PRETTY_PRINT);
+            $error->logError($e);
+            $error->sendErrorResponse($e);
         }
     }
 }
