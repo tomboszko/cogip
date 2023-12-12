6750a2a75eb12255ec9bbd20d82437566999f281
Thomas Gomree <gomreethomas@gmail.com>
2023-12-12 15:27:59 +0100

    Fix database connection issue

--- db.php
+++ db.php
@@ -12,26 +12,21 @@ require 'vendor/autoload.php';
 
 
 //heroku///
+
+
 $host = getenv('DB_HOST');
 $db   = getenv('DB_DATABASE');
 $user = getenv('DB_USERNAME');
 $pass = getenv('DB_PASSWORD');
-
-
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
+$opt = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
 ];
-
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-    //echo "Connected successfully";
-} catch (\PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
-}
+$pdo = new PDO($dsn, $user, $pass, $opt);
 
 ?>
+
