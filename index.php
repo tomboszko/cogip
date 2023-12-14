c8fb6bf4c869ce9d303e6304546c146b9181e9c3
Thomas Gomree <gomreethomas@gmail.com>
2023-12-14 11:57:46 +0100

    Update error reporting and CORS headers

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
-require 'vendor/autoload.php'; // 
+require 'vendor/autoload.php';
 
 //install whoops error handler
 $whoops = new \Whoops\Run;
@@ -12,10 +12,9 @@ $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
 // Allow CORS
-
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: application/json, Authorization");
 
 
 require 'controllers/InvoicesController.php'; // 
